2036d8724418e8b90eacedb3f9221df580b39929
DaniloYuri <>
2024-06-02 11:58:47 -0300

    configuração da api e html para conexão com o render

---
+++ API/ProcessRoutes.js
@@ -0,0 +1,26 @@
+
+        document.getElementById('processoForm').addEventListener('submit', function(event) {
+            event.preventDefault(); 
+
+            const numeroProcesso = document.getElementById('numeroProcesso').value;
+
+            fetch('https://wb-backend-48ug.onrender.com/processos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ numeroProcesso: numeroProcesso })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('A resposta da requisição não teve sucesso');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        });
