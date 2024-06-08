2fe73a7bda830d83e1ca68e38dfc10ec4a8b42ba
vitormneris <joaomoreiraneris0@gmail.com>
2024-06-08 00:54:24 -0300

    Implementação do login com sessão e inicio da implementação do menu lateral na tela sobre

---
+++ API/LoginRoutes.js
@@ -0,0 +1,38 @@
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+
+    userAuth = {
+        "email": email,
+        "password": password
+    }
+
+    fetch('http://localhost:8080/usuarios/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userAuth)
+    })
+    .then(response => response.text())
+    .then(text => {
+        let result = JSON.parse(text)
+        console.log(result)
+        if (result.message != "Not authorized") {    
+            sessionStorage.setItem("id", result)
+            window.location.href = "processo.html"
+            return "Login sucessfully"
+        }
+        return "Login not authorized"
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.error('Error:', error)
+    });
+});
