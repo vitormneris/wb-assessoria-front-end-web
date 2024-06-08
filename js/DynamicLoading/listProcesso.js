4c8bb1006f1614bb225f59d2d6a46836a9312a87
vitormneris <joaomoreiraneris0@gmail.com>
2024-06-08 18:49:10 -0300

    Listagem de processos

--- js/DynamicLoading/listProcesso.js
+++ js/DynamicLoading/listProcesso.js
@@ -1,26 +1,34 @@
 const list = document.getElementById("ListagemProcessos")
-for(i=0;i<10;i++){
-    list.innerHTML+=`
-                            <div class="card my-1">
-                                    <div class="card-body cleartfix">
-                                        <div class="media align-items-stretch">
-                                            <div class="align-self-center">
-                                                <i class="icon-wallet success font-large-2" style="padding: 0 20px 0 0; color: rgb(233, 179, 16);"></i>
-                                            </div>
-                                            <div class="media-body">
-                                                <h4>N° do Processo:</h4>
-                                                <details>
-                                                    <summary>Detalhes</summary>
-                                                    <div class="d-flex flex-column">
-                                                        <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Pendente</p>
-                                                        <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Nome</p>
-                                                        <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Ultima movimentação</p>
-                                                        <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Data e Hora</p>
-                                                    </div>
-                                                </details>
-                                            </div>
-                                        </div>
-                                    </div>
+
+let sessionData = JSON.parse(sessionStorage.getItem('data'))
+
+sessionData.forEach(element => {
+    const numeroProcesso = element.informations.numeroProcesso.numerdoDoprocesso;
+    const tribunal = element.informations.tribunal;
+    const movimentoNome = element.movimentoMaisRecente.nome;
+    const movimentoDataHora = new Date(element.movimentoMaisRecente.dataHora).toLocaleString();
+
+    list.innerHTML += `
+        <div class="card my-1">
+            <div class="card-body clearfix">
+                <div class="media align-items-stretch">
+                    <div class="align-self-center">
+                        <i class="icon-wallet success font-large-2" style="padding: 0 20px 0 0; color: rgb(233, 179, 16);"></i>
+                    </div>
+                    <div class="media-body">
+                        <h4>N° do Processo: ${numeroProcesso}</h4>
+                        <details>
+                            <summary>Detalhes</summary>
+                            <div class="d-flex flex-column">
+                                <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Tribunal: ${tribunal}</p>
+                                <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Nome: ${movimentoNome}</p>
+                                <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Última movimentação: ${movimentoNome}</p>
+                                <p class="py-2 my-1" style="width:fit-content; background-color: rgb(227, 227, 227); border-radius: 10px; padding: 0 8px;">Data e Hora: ${movimentoDataHora}</p>
                             </div>
-    `
-}
+                        </details>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+});
